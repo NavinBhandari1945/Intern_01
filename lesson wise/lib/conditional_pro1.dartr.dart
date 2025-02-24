accfe84e7780fdd4f20c6ff1de3b14fe323b6f2f
NavinBhandari1945 <navinbhandari2002@gmail.com>
2025-02-24 22:47:28 +0545

    update

---
+++ lesson wise/lib/conditional_pro1.dartr.dart
@@ -0,0 +1,13 @@
+void main(){
+  var name;
+  name="harry";
+  if(name=="harry") {
+    print("name is harry");
+  }
+  else if(name=="ram"){
+    print("i am ram");
+  }
+  else{
+    print("no name");
+  }
+}

d1da36993b040060b3a9d0a975a8ee2d7888d0b6
NavinBhandari1945 <navinbhandari2002@gmail.com>
2025-03-16 00:45:50 +0545

    datatype

---
+++ lesson wise/lib/datatype_conv1.dart
@@ -0,0 +1,14 @@
+void main(){
+  int a=12;
+  String b=a.toString();
+  print(a);
+  String c="12.5";
+  double d=double.parse(c);
+  print(d);
+  double f=10.12;
+  int e=f.toInt();
+  print(e);
+  int g=12;
+  double h=g.toDouble();
+  print(h);
+}
